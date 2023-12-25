e7bb9f6498c1f69a2dcc1246016220dd39c56304
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-25 15:16:46 +0100

    feat: implement Calibrate()

---
+++ Day_01/LibraryWeatherCalibration/WeatherCalibration.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryWeatherCalibration
+{
+    public class WeatherCalibration
+    {
+        public static object Calibrate(string v)
+        {
+            return 0;
+        }
+    }
+}

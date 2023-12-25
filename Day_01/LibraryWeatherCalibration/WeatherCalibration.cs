a00a35eec00fff6a66a3ddbba49a8f662af91699
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-25 15:45:45 +0100

    refactor: improve code structure of WeatherCalibration

--- Day_01/LibraryWeatherCalibration/WeatherCalibration.cs
+++ Day_01/LibraryWeatherCalibration/WeatherCalibration.cs
@@ -4,27 +4,33 @@
     {
         public static int Calibrate(string calibrateString)
         {
-            string result = "0";
+            return int.Parse(FindFirstNumber(calibrateString) + FindLastNumber(calibrateString));
+        }
 
+        private static string FindLastNumber(string calibrateString)
+        {
+            for (int i = calibrateString.Length; i > 0; i--)
+            {
+                if (int.TryParse(calibrateString[i - 1].ToString(), out int value))
+                {
+                    return value.ToString();
+                }
+            }
+
+            return "0";
+        }
+
+        private static string FindFirstNumber(string calibrateString)
+        {
             foreach (var item in calibrateString)
             {
                 if (int.TryParse(item.ToString(), out int value))
                 {
-                    result += item;
-                    break;
-                }                    
+                    return item.ToString();
+                }
             }
 
-            for (int i = calibrateString.Length; i > 0; i--)
-            {
-                if (int.TryParse(calibrateString[i-1].ToString(), out int value))
-                {
-                    result += value;
-                    break;
-                }                    
-            }
-
-            return int.Parse(result);
+            return "0";
         }
     }
 }
